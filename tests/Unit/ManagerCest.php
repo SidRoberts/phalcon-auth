f863fcf5bce6568d30cc9c6a142e76b8ed12b1b0
Sid Roberts <sid@sidroberts.co.uk>
2023-01-26 07:55:28 +0900

    Added empty test cest.

---
+++ tests/Unit/ManagerCest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\Support\UnitTester;
+
+class ManagerCest
+{
+    public function test(UnitTester $I): void
+    {
+        $I->markTestIncomplete();
+    }
+}
